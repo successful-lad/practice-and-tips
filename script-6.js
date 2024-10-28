40719a39c9ba18958884d01649e0f2e114d6c7e7
LetHimCook <yehor.hlebenko@oparco.de>
2024-10-28 07:57:25 +0100

    feat: add six lesson

---
+++ script-6.js
@@ -0,0 +1,51 @@
+const myName =' Yehor'
+
+const clockTime = '11pm'
+
+
+const condition = '';
+
+if ([].length) {
+  //do something
+} else {
+  //do something
+}
+
+'' //false
+
+'asdasdsd' //true
+
+12312 //true
+
+0 // false
+
+null // false
+
+undefined // false
+
+[].length // false
+
+[1, 2, 3, 4].length
+
+const additionalNameData = 'some data';
+
+const newString = `asdadad asd ${additionalNameData}`;
+
+
+newString.split(' ') // asdadad
+
+const oneString = 'asd';
+
+const secondString = 'asdasd'
+
+console.log(oneString + secondString)
+
+const key = 'skill'
+
+const person = {
+  name : 'Alex',
+  age: 19
+}
+
+console.log(person[key]) // undefined
+console.log(person['skill']) // undefined
