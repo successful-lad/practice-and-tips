4147c627bb19adbeaeff1c86be961de16025232d
LetHimCook <yehor.hlebenko@oparco.de>
2024-10-29 11:17:41 +0100

    feat: add 7 lesson

---
+++ script-7.js
@@ -0,0 +1,47 @@
+const user = {
+  name: 'Alex',
+  age: 19,
+  skills: [
+    'one skill',
+    'second skill',
+    'etc'
+  ],
+  school: {
+    name: 'asdasd',
+    city: 'asdasdada'
+  }
+}
+
+console.log(obj.name, obj['city']);
+const arr = [1, 2, 3];
+
+// delete obj.name //
+
+const newUser = {
+  ...user,
+  name: "Yehor"
+}
+
+
+
+// <p></p> // <span></span>  h1-h6  display: inline tags
+// <div></div> <section></section> display: block
+
+// <footer></footer>
+// <header></header>
+// <nav></nav>
+
+
+// <label htmlFor={'input-name'}>some label</label>
+// +
+// <input name={'asdas'} id={'input-name'}/>
+//
+
+// <button/>
+
+// <a></as>
+
+// <img ></img>
+
+// display flex
+// gap > padding || margin
